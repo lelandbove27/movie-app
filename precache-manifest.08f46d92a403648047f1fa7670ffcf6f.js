317d985cd94bb1a10a5040918ad5c2f0503b4b58
lbove27 <lbove@ucsd.edu>
2022-07-14 18:57:59 -0700

    Updates

---
+++ precache-manifest.08f46d92a403648047f1fa7670ffcf6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "710d3e635b6593fe6e49",
+    "url": "/movie-app/static/css/main.3f6ec246.chunk.css"
+  },
+  {
+    "revision": "710d3e635b6593fe6e49",
+    "url": "/movie-app/static/js/main.f85f0cf2.chunk.js"
+  },
+  {
+    "revision": "819b51ac2f20e5cf2c42",
+    "url": "/movie-app/static/js/runtime~main.8abff317.js"
+  },
+  {
+    "revision": "51aeda3ea2cee4a8b419",
+    "url": "/movie-app/static/js/2.80b1e4d2.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/movie-app/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "ff80fbf5a8194b8f7d758ecab9aa1f62",
+    "url": "/movie-app/index.html"
+  }
+];
